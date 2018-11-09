b2a436fe84ef4697670f35d9a8c2b76dec6c382a
adityachars <44857643+adityachars@users.noreply.github.com>
2018-11-09 11:48:09 +0530

    Update GoodMorning.java

--- GoodMorning.java
+++ GoodMorning.java
@@ -2,6 +2,6 @@ public class GoodMorning
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello gist @ GitHub!");   
+         System.out.println("Hello Good Morning");   
       }
 } 
